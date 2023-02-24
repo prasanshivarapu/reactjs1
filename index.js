e1a743fcc29beca6c67940d3d7374443c8ff5b80
prasan <prasan@exaple.com>
2023-02-24 08:00:19 +0000

    yeah its done

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="bg123">
+    <h1 className="heading"> super over league</h1>
+    <div className="result">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
